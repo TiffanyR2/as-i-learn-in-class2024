e593ef4ebc7f0f872124f40e331a65a5b9c7354e
Tiffanyr1 <166432598+Tiffanyr1@users.noreply.github.com>
2024-05-15 15:22:21 -0400

    first commit

---
+++ javascript/wale.js
@@ -0,0 +1,33 @@
+let kindOf = [
+    {
+        name: "Pick a Whale",
+        type:"", 
+        image:"",
+    },
+    {
+        name: "Daisy",
+        type: "Bowhead Whale",
+        image:"./images/bowhead.jpg",
+        detail: ["Lazy girl","Graceful", "Calm"]
+    },
+
+    {
+        name: "Pablo",
+        type: "Blue Whale",
+        image:"./images/blue.jpg"
+    },
+
+    {
+        name: "Maggie",
+        type: "Humpback Whale",
+        image:"./images/humpback.jpg"
+    },
+
+    {
+        name: "Jasper",
+        type: "Belugga Whale",
+        image:"./images/beluga.jpg"
+
+    },
+
+];
